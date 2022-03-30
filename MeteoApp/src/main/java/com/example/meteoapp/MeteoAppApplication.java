676201ae3db2ace93f0c0128b9dacec2c1abf8c4
Tiago Cruz <tiago-emanuel.goncalvesdacruz@atos.net>
2022-03-31 00:54:59 +0100

    first commit

---
+++ MeteoApp/src/main/java/com/example/meteoapp/MeteoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.meteoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoAppApplication.class, args);
+    }
+
+}
